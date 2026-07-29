4d0ab31c3d5ef60416eb31a19f449ed02be63400
ARCUltra121 <rfrancis121@gmail.com>
2026-07-29 10:03:58 -0400

    Adding new module -main.py to Error Handling section

--- Python/Lesson 06 - Error Handling/main.py
+++ Python/Lesson 06 - Error Handling/main.py
@@ -1,6 +1,14 @@
 from area import rect_area
+try:
+    len = float(input("Enter the length:  "))
+    wid = float(input("Enter the width:  "))
 
-len = float(input("Enter the length:  "))
-wid = float(input("Enter the width:  "))
+    rect_area(len, wid)
 
-rect_area(len, wid)
+    def function:
+        help = 'help me bro'
+
+except ValueError as ImproperValueError:
+    print("Bro give me an actual number... Please...")
+except SyntaxError as WeirdCodeError:
+    print('Code properly please my bro...')
